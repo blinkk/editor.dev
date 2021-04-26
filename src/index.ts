023f29ab08182a4fc5205a807f6b3a7d4b2a136f
Randy Merrill <randy@blinkk.com>
2021-04-26 12:01:29 -0600

    Improving the index script to be a full command.
    Supports providing a path for the local project and a custom port.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-
+import * as packageData from '../package.json';
+import {Command, Option} from 'commander';
 import {LocalApi} from './api/localApi';
 import {LocalStorage} from './storage/localStorage';
 import cors from 'cors';
 import express from 'express';
 
 const PORT = process.env.PORT || 9090;
-const args = process.argv.slice(2);
-const originHosts = [
+const ORIGIN_HOSTS = [
   'https://editor.dev',
   'https://beta.editor.dev',
   'http://localhost:8080',
@@ -19,16 +19,32 @@ const app = express();
 // Cors for communicating with editor.dev.
 app.use(
   cors({
-    origin: originHosts,
+    origin: ORIGIN_HOSTS,
   })
 );
 
-// Running as a command uses the local storage and api.
-const storage = new LocalStorage(args.length ? args[0] : undefined);
-const localApi = new LocalApi(storage);
-app.use('/', localApi.apiRouter);
+const program = new Command('npx @blinkk/editor.dev');
+program.version(packageData.version);
+program.arguments('[path]');
+program.addOption(
+  new Option('-p, --port', 'port to serve the local API from.').default(PORT)
+);
+program.action((path, options) => {
+  // Running as a command uses the local storage and api.
+  const storage = new LocalStorage(path);
+  const localApi = new LocalApi(storage);
+  const port = options.port || PORT;
+  app.use('/', localApi.apiRouter);
 
-app.listen(PORT, () => {
-  console.log(`Running on port ${PORT}`);
-  console.log(`Visit https://editor.dev/local/${PORT}/ to start editing.`);
+  app.listen(port, () => {
+    if (port !== PORT) {
+      console.log(`Running on port ${port}`);
+    }
+    console.log(
+      `Visit https://editor.dev/local/${
+        port === PORT ? '' : `${port}/`
+      } to start editing.`
+    );
+  });
 });
+program.parse(process.argv);
