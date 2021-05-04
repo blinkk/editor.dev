bb3c6f53ccbe47082bc5a47efbe386cf7210e531
Randy Merrill <randy@blinkk.com>
2021-05-04 09:10:26 -0600

    Adding simple tests for import yaml class.

--- src/utility/yamlSchemas.test.ts
+++ src/utility/yamlSchemas.test.ts
@@ -1,6 +1,19 @@
-import {ANY_SCHEMA, UnknownTag} from './yamlSchemas';
+import {ANY_SCHEMA, ImportYaml, UnknownTag} from './yamlSchemas';
+import {MemoryStorage} from '../storage/memoryStorage';
 import test from 'ava';
 import yaml from 'js-yaml';
+
+test('import yaml functionality', t => {
+  const importYaml = new ImportYaml(
+    new MemoryStorage(),
+    '/foo/bar.yaml?baz',
+    {}
+  );
+
+  t.is(importYaml.rawPath, '/foo/bar.yaml?baz');
+  t.is(importYaml.deepPath, 'baz');
+  t.is(importYaml.path, '/foo/bar.yaml');
+});
 
 test('any schema parses unknown tags', t => {
   t.deepEqual(
