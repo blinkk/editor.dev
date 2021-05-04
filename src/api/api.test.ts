6af865249dcd21e5a0bb409857a9e2c07c8dd6e0
Randy Merrill <randy@blinkk.com>
2021-05-04 10:14:03 -0600

    Basic tests for the api.

---
+++ src/api/api.test.ts
@@ -0,0 +1,38 @@
+import {GenericApiError, expandWorkspaceBranch, isWorkspaceBranch} from './api';
+import test from 'ava';
+
+test('GenericApiError properties', t => {
+  const error = new GenericApiError('foo', {
+    message: 'test',
+  });
+
+  t.is(error.message, 'foo');
+  t.is(error.apiError.message, 'test');
+});
+
+test('workspace branch expansion', t => {
+  // Special branches.
+  t.is(expandWorkspaceBranch('master'), 'master');
+  t.is(expandWorkspaceBranch('main'), 'main');
+  t.is(expandWorkspaceBranch('staging'), 'staging');
+
+  // Normal workspace branches.
+  t.is(expandWorkspaceBranch('foo'), 'workspace/foo');
+  t.is(expandWorkspaceBranch('bar'), 'workspace/bar');
+});
+
+test('is workspace branch', t => {
+  // Special branches.
+  t.is(isWorkspaceBranch('master'), true);
+  t.is(isWorkspaceBranch('main'), true);
+  t.is(isWorkspaceBranch('staging'), true);
+
+  // Normal workspace branches.
+  t.is(isWorkspaceBranch('workspace/foo'), true);
+  t.is(isWorkspaceBranch('workspace/bar'), true);
+
+  // Non-workspace branches.
+  t.is(isWorkspaceBranch('feature/foo'), false);
+  t.is(isWorkspaceBranch('bug/bar'), false);
+  t.is(isWorkspaceBranch('foogoo'), false);
+});
